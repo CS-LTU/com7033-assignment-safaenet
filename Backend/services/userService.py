71ea2a47fac7b1fb7e5d35bac6675ae7b9393c90
safaenet <safa.dh@gmail.com>
2024-10-29 12:00:57 +0000

    some bugfix applied

--- Backend/services/userService.py
+++ Backend/services/userService.py
@@ -3,13 +3,15 @@ from dal.usersDAL import UserDAL
 from models.userModel import User
 
 class UserService:
+    salt = "1q2w3e4r"
+    
     def __init__(self):
         self.user_dal = UserDAL()
 
     def signup_user(self, email, password):
         if self.user_dal.findUserByEmail(email):
             return None, "User already exists"
-        hashed_password = generate_password_hash(password)
+        hashed_password = generate_password_hash(password + self.salt)
         new_user = User(email=email, password=hashed_password)
         _id = self.user_dal.createUser(new_user.toDict())
         new_user._id = _id
@@ -19,6 +21,6 @@ class UserService:
         user_data = self.user_dal.findUserByEmail(email)
         if not user_data:
             return None, "User not found"
-        if not check_password_hash(user_data["password"], password):
+        if not check_password_hash(user_data["password"], password + self.salt):
             return None, "Incorrect password"
         return User.fromDict(user_data), None
